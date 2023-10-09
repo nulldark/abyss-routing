6cc50422ba9116a9f678895ff75214e7ffa0044e
Dominik Szamburski <dominikszamburski99@gmail.com>
2023-10-09 18:07:46 +0200

    feat: add method to compile route in route class

--- src/Routing/Route.php
+++ src/Routing/Route.php
@@ -4,11 +4,17 @@ namespace Nulldark\Routing;
 
 class Route
 {
+    /** @var string $path */
     private string $path;
+    /** @var string[] */
+    private array $methods;
+    /** @var CompiledRoute|null $compiled */
+    private ?CompiledRoute $compiled;
 
-    public function __construct(string $path)
+    public function __construct(string $path, array $methods = [])
     {
         $this->setPath($path);
+        $this->setMethods($methods);
     }
 
     /**
@@ -18,6 +24,7 @@ class Route
     public function setPath(string $path): self
     {
         $this->path = '/' . ltrim(trim($path), '/');
+        $this->compiled = null;
 
         return $this;
     }
@@ -28,5 +35,34 @@ class Route
     public function getPath(): string
     {
         return $this->path;
+    }
+
+    /**
+     * @param string|array $methods
+     * @return $this
+     */
+    public function setMethods(string|array $methods): self
+    {
+        $this->methods = array_map('strtoupper', (array) $methods);
+        $this->compiled = null;
+
+        return $this;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getMethods(): array
+    {
+        return $this->methods;
+    }
+
+    public function compile(): CompiledRoute
+    {
+        if ($this->compiled === null) {
+            $this->compiled = RouteCompiler::compile($this->getPath());
+        }
+
+        return $this->compiled;
     }
 }
